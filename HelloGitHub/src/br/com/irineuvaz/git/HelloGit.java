c6f4e269b333dc397ea363e6272e2e19834bc58c
irivaz2017 <irivaz@gmail.com>
2021-01-03 11:26:14 -0300

    Adicionei arquivos via upload

---
+++ HelloGitHub/src/br/com/irineuvaz/git/HelloGit.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.com.irineuvaz.git;
+
+/**
+ * @author irineuv
+ * Exemplo de integração com o github
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Git");
+
+	}
+
+}
